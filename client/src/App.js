505c8e048da3eb2a5e5adc6931d6d83a80a82219
bedo <bedo.ahmed416@gmail.com>
2023-01-20 22:47:57 +0200

    test again

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SlideRoutes from 'react-slide-routes';
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import ScrollToTop from "./components/scrollTotop";
 import Navbar from "./components/Navbar";
 import HomeScreen from "./screens/HomeScreen";
@@ -24,17 +24,13 @@ function App() {
     <div>
       <Navbar />
       <ScrollToTop>
-
-        {/* <SlideRoutes location={location} duration={400} > */}
-          <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
+        <SlideRoutes location={location} duration={400} >
+            <Route path="/" element={<HomeScreen />}  exact />
             <Route path="/systems" element={<SystemsScreen />} />
             <Route path="/systems/On Grid" element={<OnGridScreen />} />
             <Route path="/systems/Off Grid" element={<OffGridScreen />} />
             <Route path="/systems/Hybrid" element={<HybridScreen />} />
-          </Routes>
-        {/* </SlideRoutes> */}
-
+        </SlideRoutes>
       </ScrollToTop>
     </div>
   )
