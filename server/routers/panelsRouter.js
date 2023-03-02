15c0989f107d4110c4a46c8048fac639bc99db41
bedo <bedo.ahmed416@gmail.com>
2023-03-03 01:09:55 +0200

    fix panels bugs

--- server/routers/panelsRouter.js
+++ server/routers/panelsRouter.js
@@ -32,8 +32,9 @@ panelRouter.post(
     panels = panels.map(x => {
       return { id: x._id, name: x.name, power: x.power, vmpp: x.vmpp, impp: x.impp, voc: x.voc, isc: x.isc, dimensions: x.dimensions, price: x.price, efficiency: x.efficiency }
     })
-    const {energy,inverter,loss,peakSonHours,expectArea} = req.body
-    let response = chosePanels({energy,inverter,loss,peakSonHours,panels,
+    const { energy, inverter, loss, peakSonHours, expectArea } = req.body
+    let response = chosePanels({
+      energy, inverter, loss, peakSonHours, panels,
       tiltAngle: 28,
       AzimuthAngle: 23,
       expectArea
@@ -67,7 +68,8 @@ function chosePanels(data) {
   let panelsPower = energy / peakSonHours || energy / 5
   for (let panel of panels) {
     let numOfPanels = Number((panelsPower / panel.power).toFixed(0))
-    numOfPanels = numOfPanels % 2 !== 0 ? numOfPanels + 1 : numOfPanels
+    numOfPanels = numOfPanels > 0 ? numOfPanels : numOfPanels + 1
+    // numOfPanels = numOfPanels % 2 !== 0 && numOfPanels !== 1 ? numOfPanels + 1 : numOfPanels
     let totalPrice = numOfPanels * panel.price
     let height = Math.max((panel.dimensions.width / 1000), (panel.dimensions.height / 1000))
     height = (height * Math.cos(tiltAngle)) + ((height * Math.sin(tiltAngle)) / Math.tan(AzimuthAngle))
@@ -86,8 +88,8 @@ function chosePanels(data) {
   }
   // console.log(shPanels)
   for (let panel of shPanels) {
-    // let sum = shPanels.reduce((b,a)=>a.numOfPanels + b, 0)
-    let sum = Math.max(...shPanels.map(x => x.numOfPanels))
+    let sum = shPanels.reduce((b, a) => a.numOfPanels + b, 0)
+    // let sum = Math.max(...shPanels.map(x => x.numOfPanels))
     let numScore = 100 - ((panel.numOfPanels / sum) * 100)
     sum = Math.max(...shPanels.map(x => (100 - ((x.numOfPanels / sum) * 100))))
     numScore = (numScore / sum) * 100
@@ -103,7 +105,7 @@ function chosePanels(data) {
     areaScore = (areaScore / maxArea) * 100
     // console.log( closeTo(shPanels.map(x=> x.totalArea),panel.totalArea,expectArea))
     let totalScore = (priceScore + numScore + areaScore) / 3
-    // console.log(panel.id, "n "+panel.numOfPanels, "nS "+numScore.toFixed(2), "p "+panel.totalPrice, "pS "+priceScore.toFixed(2), "A "+panel.totalArea.toFixed(2),"AS "+areaScore.toFixed(2), "T "+totalScore.toFixed(2))
+    // console.log(panel.id, "n " + panel.numOfPanels, "nS " + numScore.toFixed(2), "p " + panel.totalPrice, "pS " + priceScore.toFixed(2), "A " + panel.totalArea.toFixed(2), "AS " + areaScore.toFixed(2), "T " + totalScore.toFixed(2))
 
     score.push({
       ...panel,
